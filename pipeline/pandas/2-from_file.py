f27e38ee1bfd3bb90cd8cafa8a6ddf00b62eb01a
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-05 13:46:57 +0000

    asdhbas

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""loading data from array"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """loading data from array"""
+    return pd.read_csv("filename", delimiter=sep)
