73c8d27546c2ec71a73cd37588b4e2ea004aedb8
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-05 14:22:16 +0000

    pandas

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """taking input and performing"""
 import pandas as pd
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df = df.rename(columns={'Timestamp': "Datetime"})
 df['Datetime'] = pd.date_time(df['Datetime'], unit='s')
