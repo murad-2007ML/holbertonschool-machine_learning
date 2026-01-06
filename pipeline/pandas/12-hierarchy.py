3214c5cc7d1e83ecfbdd90c47ab9ab27370bb2a6
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-06 13:55:01 +0000

    pandas

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 """concat indexses with a multiindex"""
-index = __import__('10-index').index
 import pandas as pd
+index = __import__('10-index').index
 
 
 def hierarchy(df1, df2):
     """
     Rearranges the MultiIndex so that Timestamp is the first level.
-    Concatenates the bitstamp and coinbase tables from timestamps 1417411980 to 1417417980, inclusive.
-    Adds keys to the data, labeling rows from df2 as bitstamp and rows from df1 as coinbase.
-    Ensures the data is displayed in chronological order
+    Concatenates bitstamp and coinbase from 1417411980 to 1417417980.
     """
-    df1 = df1.loc[
-        (df1["Timestamp"] >= 1417411980) & (df1["Timestamp"]) <= 1417417980]
-    df2 = df2.loc[
-        (df2["Timestamp"] >= 1417411980) & (df2["Timestamp"]) <= 1417417980]
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
+
     df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
-    df = df.sort_index()
+    df = df.swaplevel(0, 1).sort_index()
+
     return df
