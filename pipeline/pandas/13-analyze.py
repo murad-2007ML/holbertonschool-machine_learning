77f85779ef26d6deef7e93071818a70de993a0e0
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-07 12:10:07 +0000

    pandas

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -6,5 +6,4 @@ def analyze(df):
     """Computes descriptive statistics for all columns except the Timestamp column.
     Returns a new pd.DataFrame containing these statistics
     """
-    df = df.drop(columns="TimeStamp").describe()
-    return df
+    return df.drop(columns="TimeStamp").describe()
