bb28f4137f001dcdf7014fbccd92a7527d36b679
murad-2007Ml <haliqhuseynov2000@gmail.com>
2026-01-09 12:43:38 +0000

    scale

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
-"""x->y1 x->y2"""
+"""Docstring for 3-two.py."""
 import numpy as np
+
+
 import matplotlib.pyplot as plt
 
 
 def two():
-    """
-    The x-axis should be labeled Time (years)
-
-    The y-axis should be labeled Fraction Remaining
-
-    The title should be Exponential Decay of Radioactive Elements
-
-    The x-axis should range from 0 to 20,000
-
-    The y-axis should range from 0 to 1
-
-    x ↦ y1 should be plotted with a dashed red line
-
-    x ↦ y2 should be plotted with a solid green line
-
-    A legend labeling x ↦ y1 as C-14 and x ↦ y2 as Ra-226 should be placed in the upper right hand corner of the plot
-    """
+    """Docstring for two."""
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
     t1 = 5730
@@ -29,11 +15,11 @@ def two():
     y1 = np.exp((r / t1) * x)
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
     plt.xlabel("Time (years)")
     plt.ylabel("Fraction Remaining")
     plt.title("Exponential Decay of Radioactive Elements")
-    plt.xlim(0, 20000)
-    plt.ylim(0, 1)
-    plt.plot(x, y1, 'r--', label="C-14")
-    plt.plot(x, y2, 'g-', label="Ra-226")
-    plt.legend(loc="upper right")
+    plt.plot(x, y1, color="red", linestyle="--")
+    plt.plot(x, y2, color="green")
+    plt.legend(["C-14", "Ra-226"], loc="upper right")
