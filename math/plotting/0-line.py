6aa37369f16f84b79c1b9f4b54d53c78a247eb43
murad-2007Ml <haliqhuseynov2000@gmail.com>
2026-01-09 12:09:46 +0000

    plotting

---
+++ math/plotting/0-line.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" to plot y as a line graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    """
+    y should be plotted as a solid red line
+    The x-axis should range from 0 to 10
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 10)
+    plt.plot(y, color="red")
