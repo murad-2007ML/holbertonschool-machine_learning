ea21a94d5858dc8a843fb9e3cfed30f7787109c3
murad-2007Ml <haliqhuseynov2000@gmail.com>
2026-01-09 13:46:35 +0000

    bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
-"""stacked bar graph"""
+"""Docstring for 6-bars.py."""
 import numpy as np
+
+
 import matplotlib.pyplot as plt
 
 
 def bars():
-    """
-    representing the number of fruit various people possess
-    """
+    """Docstring for bars."""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
+    categories = ["Farrah", "Fred", "Felicia"]
+    level1 = fruit[0]
+    level2 = fruit[1]
+    level3 = fruit[2]
+    level4 = fruit[3]
     plt.figure(figsize=(6.4, 4.8))
-    people = ("Farah", "Fred", "Felicia")
-    plt.bar(people, fruit[0], width=0.5, color='red', label='apples')
-    plt.bar(people, fruit[1], width=0.5, color='yellow',
-            bottom=fruit[0], label='bananas')
-    plt.bar(people, fruit[2], width=0.5, color='#ff8000',
-            bottom=fruit[0] + fruit[1], label='oranges')
-    plt.bar(people, fruit[3], width=0.5, color='#ffe5b4',
-            bottom=fruit[0] + fruit[1] + fruit[2], label='peaches')
-    plt.yticks(np.arange(0, 81, 10))
-    plt.ylim(0, 80)
-    plt.ylabel("Quantity of Fruit")
     plt.title("Number of Fruit per Person")
+    plt.ylabel("Quantity of Fruit")
+    plt.ylim(0, 80)
+    list1 = level1
+    list2 = level1 + level2
+    list3 = level1 + level2 + level3
+    plt.bar(categories, level1, color="red", width=0.5)
+    plt.bar(categories, level2, bottom=list1, color="yellow", width=0.5)
+    plt.bar(categories, level3, bottom=list2, color="#ff8000", width=0.5)
+    plt.bar(categories, level4, bottom=list3, color="#ffe5b4", width=0.5)
     plt.legend(["apples", "bananas", "oranges", "peaches"])
-    plt.show()
