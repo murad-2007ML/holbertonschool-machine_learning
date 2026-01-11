70d7f999188598175bfd0534535c2d8a88dc497f
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-11 10:50:21 +0000

    gradient

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -18,9 +18,10 @@ def gradient():
     y = np.random.randn(2000) * 10
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
     plt.figure(figsize=(6.4, 4.8))
+    scatter = plt.scatter(x, y, c=z)
     plt.xlabel("x coordinate (m)")
     plt.ylabel("y coordinate (m)")
     plt.title("Mountain Elevation")
-    plt.colorbar().set_label("elevation (m)")
-    plt.scatter(x, y, c=z)
+    cbar = plt.colorbar(scatter)
+    cbar.set_label("elevation (m)")
     plt.show()
