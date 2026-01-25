d43f2caf8b29a4ec699eed2ac9a3f5db6c586775
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-25 18:21:34 +0000

    accriss_planes

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""adds two arrays element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    You can assume that mat1 and mat2 are 2D matrices containing ints/floats
+
+    You can assume all elements in the same dimension are of the same type/shape
+
+    You must return a new matrix
+
+    If mat1 and mat2 are not the same shape, return None
+    """
+    new = []
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    for i in range(len(mat1)):
+        matrix_element = []
+        for j in range(len(mat1[0])):
+            matrix_element.append(mat1[i][j] + mat2[i][j])
+        new.append(matrix_element)
+    return new
