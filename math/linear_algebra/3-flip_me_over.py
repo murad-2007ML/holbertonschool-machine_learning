5abe3b23fa9e3b5f81967d443f25173aa37d5e7d
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-25 18:02:42 +0000

    flip

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""transpose of matrix"""
+
+
+def matrix_transpose(matrix):
+    """
+    You must return a new matrix
+    You can assume that matrix is never empty
+    You can assume all elements in the same dimension are of the same type/shape
+    """
+    new = []
+    for i in range(len(matrix[0])):
+        matrix_element = []
+        for j in range(len(matrix)):
+            matrix_element.append(matrix[j][i])
+        new.append(matrix_element)
+    return new
