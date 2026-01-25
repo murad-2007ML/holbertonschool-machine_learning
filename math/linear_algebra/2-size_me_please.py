6019313c9c9a1e13181fccd665872b5117b63548
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-25 17:52:12 +0000

    size

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-"""calculates the shape of a matrix"""
+"""Calculates the shape of a matrix"""
 
 
 def matrix_shape(matrix):
     """
-    You can assume all elements in the same dimension are of the same type/shape
+    You can assume all elements in the same dimension are of the
+    same type/shape
+
     The shape should be returned as a list of integers
     """
     shape = []
