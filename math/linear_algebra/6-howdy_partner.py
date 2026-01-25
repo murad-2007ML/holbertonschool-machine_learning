443651fc3a452a8a2b6618ab19c87a0c62b65cbe
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-25 18:25:38 +0000

    howdy

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    You can assume that arr1 and arr2 are lists of ints/floats
+
+    You must return a new list
+    """
+    new = []
+    new = arr1 + arr2
+    return new
