c62cbf8323281962edb16f4c6c60ef15fa02ac64
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-26 12:41:20 +0000

    linear_algebra

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-""" performs matrix multiplication"""
+"""Performs matrix multiplication"""
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """
-    You can assume that mat1 and mat2 are numpy.ndarrays
-
-    You are not allowed to use any loops or conditional statements
-
-    You may use: import numpy as np
-
-    You can assume that mat1 and mat2 are never empty
+    Multiplies two matrices using numpy.
+    Returns a new numpy.ndarray.
     """
-    multiple = np.dot(mat1, mat2)
-    return multiple
+    return np.matmul(mat1, mat2)
