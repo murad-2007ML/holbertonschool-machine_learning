0b9d00b03fea96d65404186929ae33f81b91c06a
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-26 11:40:28 +0000

    gettin_cozy

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,19 +4,15 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """
-    You can assume that mat1 and mat2 are 2D matrices containing ints/floats
-
-    You can assume all elements in the same dimension are of the same type/shape
-
-    You must return a new matrix
-
-    If the two matrices cannot be concatenated, return None
+    Concatenates two 2D matrices along a specific axis.
+    Returns a new matrix, or None if the operation is impossible.
     """
-    if (len(mat1[0]) == len(mat2[0])) and axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         new_mat1 = [i[:] for i in mat1]
         new_mat2 = [i[:] for i in mat2]
         return new_mat1 + new_mat2
-    elif (len(mat1) == len(mat2)) and axis == 1:
-        new_matrix = [mat1[i] + mat2[i] for i in range(len(mat1))]
-        return new_matrix
+
+    if axis == 1 and len(mat1) == len(mat2):
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+
     return None
