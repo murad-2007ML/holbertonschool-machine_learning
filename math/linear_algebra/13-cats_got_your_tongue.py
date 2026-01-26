2d4e3a2657c4dcbb6f370c784b65055166443b5d
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-26 12:34:37 +0000

    linear_algebra

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" concatenates two matrices along a specific axis"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    You can assume that mat1 and mat2 can be interpreted as numpy.ndarrays
+
+    You must return a new numpy.ndarray
+
+    You are not allowed to use any loops or conditional statements
+
+    You may use: import numpy as np
+
+    You can assume that mat1 and mat2 are never empty
+    """
+    new_mat = np.transpose((mat1, mat2), axis)
+    return new_mat
