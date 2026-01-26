a2d47393a07ef8e3b03919961b120ff5bb154d25
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-01-26 12:05:03 +0000

    ridin_bareback

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
-"""performs matrix multiplication"""
+"""Performs matrix multiplication"""
 
 
 def mat_mul(mat1, mat2):
     """
-    You can assume that mat1 and mat2 are 2D matrices containing ints/floats
-
-    You can assume all elements in the same dimension are of the same type/shape
-
-    You must return a new matrix
-
-    If the two matrices cannot be multiplied, return None
+    Multiplies two 2D matrices.
+    Returns a new matrix, or None if the operation is impossible.
     """
+    # Check if inner dimensions match (Cols of mat1 == Rows of mat2)
     if len(mat1[0]) != len(mat2):
         return None
-    else:
-        new_mat = []
-        for i in range(len(mat1)):
-            mat_i = []
-            for j in range(len(mat2[0])):
-                vec = 0
-                for k in range(len(mat2)):
-                    vec += mat1[i][k] * mat2[k][j]
-                mat_i.append(vec)
-            new_mat.append(mat_i)
-        for x in new_mat:
-            return new_mat
+
+    # Initialize the result matrix with dimensions: len(mat1) x len(mat2[0])
+    new_mat = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            # Calculate the dot product of row i and column j
+            dot_product = 0
+            for k in range(len(mat2)):
+                dot_product += mat1[i][k] * mat2[k][j]
+            row.append(dot_product)
+        new_mat.append(row)
+
+    return new_mat
