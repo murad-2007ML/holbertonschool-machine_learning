78bfd98b7dc1d75ecddaef40baf2e8469c47d54a
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-01 12:24:57 +0000

    linear

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -4,19 +4,24 @@
 
 def matrix_shape(matrix):
     """ returns list of integers representing dimensions of given matrix """
-
-    matrix_shape = []
-    while type(matrix) is list:
-        matrix_shape.append(len(matrix))
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
         matrix = matrix[0]
-    return matrix_shape
+    return shape
 
 
 def add_matrices(mat1, mat2):
     """ returns new matrix that is sum of two matrices added element-wise """
-
+    # Check if shapes are identical
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    if len(matrix_shape(mat1)) is 1:
-        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+
+    # Base case: if elements are not lists, they are numbers to be added
+    if not isinstance(mat1, list):
+        return mat1 + mat2
+
+    # Recursive step: iterate through the lists and call add_matrices
     return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
