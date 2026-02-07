cfe4b181696048223ce0e8344b9c9b9029cd9050
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-07 19:28:03 +0400

    poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""a class Poisson that represents a poisson distribution"""
+
+
+class Poisson:
+    """
+    class to call methods of Poisson distribution
+    """
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        data: list of data to estimate distribution
+        lambtha: number of occurences in a given time frame
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = (sum(data) / len(data))
