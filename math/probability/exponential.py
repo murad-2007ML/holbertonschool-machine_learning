6e231bd91cd7a8acae503ad5c7ad18c037f46d34
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-09 22:11:31 +0400

    exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Script to calculate a Exponential distribution
+"""
+
+
+class Exponential:
+    """
+    class to call the methods of exponential distribution
+    """
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        init
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = (1 / (sem(data) / len(data)))
