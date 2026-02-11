ae97e47821a577c32741a8107166034bdff9f552
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-11 17:00:32 +0400

    posterior

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -97,3 +97,31 @@ def marginal(x, n, P, Pr):
     for i in inter:
         s += i
     return s
+
+
+def posterior(x, n, P, Pr):
+    '''
+    calculating the posterior probability
+    '''
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        error = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(error)
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
+    inter = (combination(n, x) * (P ** x) * ((1 - P) ** (n - x))) * Pr
+    s = 0
+    for i in inter:
+        s += i
+    return inter / s
