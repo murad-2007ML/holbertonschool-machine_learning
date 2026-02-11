eaa9169ff0bbd989160fa37058e2561d9ab7369c
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-11 15:17:41 +0400

    cdf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -60,3 +60,15 @@ class Binomial:
         pi = self.p
         res = fac(n)/(fac(k)*fac(n-k))*(pi**k)*((1-pi)**(n-k))
         return res
+
+        def cdf(self, k):
+        """
+        calculating cdf
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+        res = 0
+        for i in range(k+1):
+            res += self.pmf(i)
+        return res
