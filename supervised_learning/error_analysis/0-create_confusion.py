1b02e9eece299040db1b3530d20fb101a7033c1d
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-02-16 15:37:33 +0400

    confusion

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Returning confusion matrix
+    """
+    return np.matmul(labels.T, logits)
