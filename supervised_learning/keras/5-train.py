022d7f8225929df45b57d0d83b145d31919eda72
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-14 13:01:45 +0400

    train

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,7 +4,7 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, 
+def train_model(network, data, labels, batch_size,
                 epochs, validation_data=None, verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent."""
     history = network.fit(
