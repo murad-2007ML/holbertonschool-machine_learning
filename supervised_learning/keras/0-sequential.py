857b3bfaaafbb93ebd59ae34d5b835aa9b7963b4
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-14 11:40:52 +0400

    sequential

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Script for build a DNN in keras"""
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    sequential with keras
+    """
+    model = K.Sequential()
+    L2 = K.regularizers.l2(lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], input_shape=(nx,)
+                                     activation=activations[i]
+                                     kernel_regularizer=L2
+                                     name='dense'))
+        else:
+            model.add(K.layers.Dropout(1 - keep_prob))
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=L2,
+                                     name='dense_' + str(i)))
+    return model
