320c35240e02379e88af8d4e01c67dfed7773a72
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-14 12:06:01 +0400

    train

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Script to train a model using keras"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """
+    building neural networks with keras
+    """
+    history = network.fit(data, labels, epochs=epochs
+                          batch_size=batch_size, verbose=verbose
+                          shuffle=shuffle)
+    return history
