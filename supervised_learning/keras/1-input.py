566d5ddbd572b0ed2ac2eaed567b9d8eaeef92bb
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-14 11:52:48 +0400

    inputs

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -5,18 +5,23 @@ import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """
-    building neural network with keras
-    """
-    inputs = K.input(shape=(nx,))
+    """building neural network with keras"""
+    inputs = K.Input(shape=(nx,))
     L2 = K.regularizers.l2(lambtha)
+
     for i in range(len(layers)):
         if i == 0:
-            output = K.layers.Dense(layers[i],
-                                    activation=activations[i],
-                                    kernel_regularizer=L2)(inputs)
+            output = K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=L2
+            )(inputs)
         else:
             dropout = K.layers.Dropout(1 - keep_prob)(output)
-            output = K.layers.Dense(layers[i], activation=activations[i],
-                                    kernel_regularizer=L2)(dropout)
-    return K.models.Model(inputs=inputs, outputs=output)
+            output = K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=L2
+            )(dropout)
+
+    return K.Model(inputs=inputs, outputs=output)
