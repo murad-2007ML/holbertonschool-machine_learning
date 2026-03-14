7a92b0c8dd520754c838cc949d5a2f76000b2bf2
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-14 12:56:18 +0400

    train

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-"""Script to train a model using keras."""
+'''
+training the data
+'''
+
 
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                verbose=True, shuffle=False):
-    """Trains a model using mini-batch gradient descent."""
+def train_model(network, data, labels, batch_size, 
+                epochs, verbose=True, shuffle=False):
+    '''
+    network - the model to train
+    data - ndarray of shape (m, nx) containing the input data
+    labels - one-hot ndarray of chape (m, classes) containing labels of data
+    batch_size - is the size of the batch used for mini-batch gradient descent
+    epochs - the number of passes through data for mini-batch gradient descent
+    verbose - the boolean that determines if output should be printed
+
+    shuffle - a boolean that determines whether to shullfe batches every epoch
+    Normally, it is a good idea to shuffle, but for reproducibility, 
+    we have chosen to set the default to False
+
+    returns: History object generated after training the model
+    '''
+
     history = network.fit(
-        data,
-        labels,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle
+            x=data,
+            y=labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1 if verbose else 0,
+            shuffle=shuffle
     )
+
     return history
