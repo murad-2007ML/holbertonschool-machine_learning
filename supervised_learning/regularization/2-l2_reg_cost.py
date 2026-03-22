2baab3278bca3e2a4cdf38e8572ab3659b5aabb9
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-22 13:34:24 +0400

    layers

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 '''
-regularization_cost_with_L2
+creating new layers
 '''
 
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, model):
+def l2_reg_create_layer(prev, n, activation, lambd):
     """
-    cost - tensor containing the cost of the network without L2 regularization
-    model - a Keras model that includes layers with L2 regularization
+    prev is a tensor containing the output of the previous layer
+    n is the number of nodes the new layer should contain
+    activation is the activation function that should be used on the layer
+    lambtha is the L2 regularization parameter
     """
-    reg_loss = [tf.add_n(lay.losses) for lay in model.layers if lay.losses]
-    return cost + reg_loss
+    layer = tf.keras.layers.Dense(
+        n,
+        activation=activation
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0,
+            mode='fan_avg'
+        ),
+    kernel_regularizer=tf.keras.regularizers.L2(lambd)
+    )
+    return layer(prev)
