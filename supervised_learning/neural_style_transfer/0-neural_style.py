5b31cffa9f41adb0d338f30a0cb4f07bbd63e280
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-05-04 17:43:42 +0400

    neural_style

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """NST that performs tasks for neural style transfer"""
 
-
 import numpy as np
 import tensorflow as tf
 
@@ -9,44 +8,33 @@ import tensorflow as tf
 class NST:
     """
     Performs tasks for Neural Style Transfer
-
-    public class attributes:
-        style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                        'block4_conv1', 'block5_conv1']
-        content_layer = 'block5_conv2'
-
-    instance attributes:
-        style_image: preprocessed style image
-        content_image: preprocessed style image
-        alpha: weight for content cost
-        beta: weight for style cost
-        model: the Keras model used to calculate cost
-
-    class constructor:
-        def __init__(self, style_image, content_image, alpha=1e4, beta=1)
-
-    static methods:
-        def scale_image(image):
-            rescales an image so the pixel values are between 0 and 1
-                and the largest side is 512 pixels
     """
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 
-                    'block4_conv1', 'block5_conv1']
-    content_layer = 'block5_conv2'
-    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        if type(style_image) is not np.ndarray or \
-        len(style_image.shape) != 3 or \
-        style_image.shape[2] != 3:
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
 
-        if type(content_image) is not np.ndarray or \
-        len(content_image.shape) != 3 or \
-        content_image.shape[2] != 3:
-            raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
+    style_layers = [
+        'block1_conv1', 'block2_conv1', 'block3_conv1',
+        'block4_conv1', 'block5_conv1'
+    ]
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image,
+                 alpha=1e4, beta=1):
+        if (type(style_image) is not np.ndarray or
+                len(style_image.shape) != 3 or
+                style_image.shape[2] != 3):
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
+
+        if (type(content_image) is not np.ndarray or
+                len(content_image.shape) != 3 or
+                content_image.shape[2] != 3):
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
         if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
- 
+
         if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
@@ -57,11 +45,13 @@ class NST:
 
     @staticmethod
     def scale_image(image):
-        """ scales image dimensions and values to 0-1"""
-        if type(image) is not np.ndarray or \
-            len(image.shape) != 3 or \
-            image.shape[2] != 3:
-            raise TypeError("image must be a numpy.ndarray with shape (h, w, 3)")
+        """Scales image dimensions and values to 0-1"""
+        if (type(image) is not np.ndarray or
+                len(image.shape) != 3 or
+                image.shape[2] != 3):
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)"
+            )
 
         h, w, c = image.shape
 
@@ -76,7 +66,11 @@ class NST:
 
         image = np.expand_dims(image, axis=0)
 
-        scaled_image = tf.image.resize(image, new_shape, method='bicubic')
-        scaled_image = tf.clip_by_value(scaled_image / 255, 0, 1)
+        scaled_image = tf.image.resize(
+            image, new_shape, method='bicubic'
+        )
+        scaled_image = tf.clip_by_value(
+            scaled_image / 255, 0, 1
+        )
 
         return scaled_image
