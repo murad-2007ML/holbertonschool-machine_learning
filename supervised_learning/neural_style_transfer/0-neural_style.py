36668367d1ad5ff3e11af97a7aa443295426553e
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-05-04 17:37:20 +0400

    neural_style

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -44,10 +44,10 @@ class NST:
         content_image.shape[2] != 3:
             raise TypeError("content_image must be a numpy.ndarray with shape (h, w, 3)")
 
-        if alpha < 0:
+        if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
  
-        if beta < 0:
+        if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
         self.style_image = self.scale_image(style_image)
